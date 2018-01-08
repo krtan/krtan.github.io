3816b4565f66605534fd56138557142b1214271c
jurassicplayer <krtan@g.ucla.edu>
2018-01-08 15:11:39 -0800

    Commented out old header CSS and rearranged.

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ Vue.component('navbar', {
 		}
 	},
 });
+
 Vue.component('navlink', {
 	props: ['href'],
 	template: `
@@ -39,7 +40,7 @@ Vue.component('navlink', {
 
 Vue.component('blog-list', {
 	template: `
-		<div>
+		<div class="blog-list">
 			<blog-post v-for="post in blogList" :key="post.title" :title="post.title" :date="post.date" :time="post.time" :author="post.author" :tags="post.tags">{{ post.body }}</blog-post>
 		</div>
 	`,
@@ -58,7 +59,7 @@ Vue.component('blog-list', {
 Vue.component('blog-post', {
 	props: ['title', 'date', 'time', 'author', 'tags'],
 	template: `
-		<div class="card block">
+		<article class="card block">
 			<div class="card-header">
 				<div class="card-header-title">{{ title }}</div>
 			</div>
@@ -68,17 +69,28 @@ Vue.component('blog-post', {
 				<div class="card-footer-item">{{ date }}</div>
 				<div class="card-footer-item">{{ time }}</div>
 			</div>
-		</div>
+		</article>
 	`
+});
+
+Vue.component('footer-nav', {
+	template: `
+		<div id="footer-nav">
+			<slot></slot>
+		</div>
+	`,
+});
+
+Vue.component('footer-link', {
+	props: ['icon', 'href'],
+	template: `
+		<a class="svg-icon" target="_blank" v-bind:href="href"><i class="svg-icon" v-bind:class="icon"></i></a>
+	`,
 });
 
 var app = new Vue({
 	el: '#app',
-})
-
-var main_root = new Vue({
-    el: '#mainRoot',
-    data: {
+	data: {
         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec vehicula diam. In hac habitasse platea dictumst. Nunc placerat consectetur est, nec sagittis nulla sollicitudin quis. In hac habitasse platea dictumst. Phasellus vel eros dolor. Donec a lorem ac mauris pellentesque mattis non egestas sem. Phasellus eu risus porttitor, vehicula risus nec, ultrices tellus. Sed molestie ligula ac tellus pellentesque sagittis. Phasellus eu pulvinar leo. Ut sed erat consequat, accumsan justo at, dictum sapien. Donec dictum luctus blandit. Fusce dapibus tortor sed purus egestas, sit amet faucibus mi cursus. Vivamus tempus mauris congue nisl tristique porta. Vivamus blandit imperdiet dui ac pharetra. Donec ligula diam, tristique id feugiat a, tincidunt nec purus. Donec sed varius mauris, nec tincidunt neque.\
         </////////>\
         Praesent nec elit ut lorem ultricies condimentum eu id odio. Sed non pretium massa. Nulla sed dapibus metus. Phasellus a nisi ornare, posuere neque ultrices, dapibus justo. Curabitur eu efficitur felis, sed euismod risus. Nulla luctus ante ac varius elementum. Morbi justo quam, molestie nec nisi aliquam, hendrerit malesuada purus. Nam ante odio, tempor et eleifend gravida, tempor a ligula. Sed maximus lobortis velit, a laoreet mi congue in. Morbi ac volutpat lectus. Nulla eu dui leo. Pellentesque dignissim mauris eu orci rutrum eleifend. Nulla ultricies odio a enim scelerisque, id viverra odio pretium.\
@@ -88,16 +100,5 @@ var main_root = new Vue({
         Nam vulputate purus eu nisi condimentum, a lobortis quam molestie. Nunc risus orci, congue a felis sed, hendrerit varius mauris. Cras imperdiet luctus neque nec accumsan. Nunc tincidunt, ligula a aliquam bibendum, mi arcu fringilla ante, sed luctus sapien mi eget enim. Praesent imperdiet nisl eget ipsum commodo dapibus. Donec imperdiet lectus vel fringilla posuere. Mauris posuere ornare ipsum eu faucibus. Aliquam eget eros lorem. Proin interdum quis arcu vel dapibus. Maecenas sit amet interdum leo. Pellentesque pulvinar erat in purus varius, at interdum leo mollis. Curabitur eget tempus nisi. Fusce ut ultricies magna, pellentesque aliquam tortor.\
         \
         In sodales rhoncus volutpat. Sed interdum fermentum vulputate. Donec tincidunt ligula nisl, vitae posuere leo dapibus scelerisque. Integer ullamcorper ex magna, eget porttitor nisi iaculis a. Vivamus vestibulum auctor quam, ut fermentum elit hendrerit nec. Praesent a rhoncus orci, in tincidunt erat. Sed imperdiet ex id tellus vestibulum, ut ullamcorper purus finibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    }
-})
-
-var footer_root = new Vue({
-    el: '#footerRoot',
-    data: {
-        externalURLs: [
-			{ css_icon: 'github', hyperlink: 'https://github.com/krtan' },
-			{ css_icon: 'linkedin', hyperlink: 'https://linkedin.com/in/kevin-tan-b1a40a149/' },
-			{ css_icon: 'treehouse', hyperlink: 'https://teamtreehouse.com/krtan' }
-		]
     }
 })
